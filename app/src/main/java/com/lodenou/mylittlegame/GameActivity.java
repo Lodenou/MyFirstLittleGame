614c1b75f2f3b8e2f9b10f0a511d465b7b2f55d0
Lodenou <denoual.loic.dl@gmail.com>
2020-05-30 00:23:34 +0200

    GameActivity creation
    creation + getextra from dialog

---
+++ app/src/main/java/com/lodenou/mylittlegame/GameActivity.java
@@ -0,0 +1,24 @@
+package com.lodenou.mylittlegame;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+        String characterName = intent.getStringExtra(ViewDialog.EXTRA_CHARACTER_NAME);
+
+        // Capture the layout's TextView and set the string as its text
+        TextView textViewGameActivity = findViewById(R.id.text_view_game);
+        textViewGameActivity.setText(characterName);
+    }
+}
