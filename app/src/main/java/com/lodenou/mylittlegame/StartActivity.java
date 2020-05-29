6154927985a5106a54cbb0d9f8db0374284dd256
Lodenou <denoual.loic.dl@gmail.com>
2020-05-29 19:59:03 +0200

    add dialog
    add dialog alert + layout work

--- app/src/main/java/com/lodenou/mylittlegame/StartActivity.java
+++ app/src/main/java/com/lodenou/mylittlegame/StartActivity.java
@@ -7,19 +7,25 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
-import android.media.MediaPlayer;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.os.PowerManager;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Spinner;
 
-public class StartActivity extends AppCompatActivity {
+
+public class StartActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     private boolean mIsBound = false;
     private MusicService mServ;
     ImageButton mImageButton;
+    Button new_game_button;
+    String[] mClasses = {"Guerrier","Magicien","Moine", "Ninja", "Samouraï","Paysan"};
+
 
 
 
@@ -29,12 +35,12 @@ public class StartActivity extends AppCompatActivity {
         setContentView(R.layout.activity_start);
         musicOn();
         muteSound();
+        showDialog();
 
     }
     public void muteSound() {
         mImageButton = findViewById(R.id.mute_sound_button);
         mImageButton.setOnClickListener(new View.OnClickListener() {
-            @RequiresApi(api = Build.VERSION_CODES.M)
             @Override
             public void onClick(View v) {
                     if(mServ.mPlayer.isPlaying()) {
@@ -47,6 +53,10 @@ public class StartActivity extends AppCompatActivity {
                     }
             }
         });
+    }
+
+    public void startNewGame() {
+
     }
 
     //BIND Music Service
@@ -122,5 +132,44 @@ public class StartActivity extends AppCompatActivity {
         music.setClass(this,MusicService.class);
         stopService(music);
 
+    }
+
+    private void initiateSpinner() {
+        //SPINNER SETTINGS
+
+        //Getting the instance of Spinner and applying OnItemSelectedListener on it
+        final Spinner spin = (Spinner) findViewById(R.id.spinner);
+        spin.setOnItemSelectedListener(this);
+
+        //Creating the ArrayAdapter instance having the rooms' list
+        ArrayAdapter aa = new ArrayAdapter(this, android.R.layout.simple_spinner_item, mClasses);
+        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //Setting the ArrayAdapter data on the Spinner and nothingselected
+        spin.setAdapter(new NothingSelectedSpinnerAdapter(
+                aa,
+                R.layout.contact_spinner_row_nothing_selected,
+                this));
+        spin.setPrompt("Selectionnez une salle");
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+    public void showDialog(){
+        new_game_button = findViewById(R.id.first_button);
+        new_game_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ViewDialog alert = new ViewDialog();
+                alert.showDialog(StartActivity.this,"");
+            }
+        });
     }
 }
